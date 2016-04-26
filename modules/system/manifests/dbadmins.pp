490d61c187c466ae302047f5bc322d9609821956
paul <paul@puppetlabs.vm>
2016-04-26 20:11:26 +0000

    added 'system' module

---
+++ modules/system/manifests/dbadmins.pp
@@ -0,0 +1,49 @@
+class system::dbadmins {
+
+  $user = [
+    'zack',
+    'monica',
+    'ralph',
+    'brad',
+    'luke',
+  ]
+
+  require mysql::server
+
+  $user.each |$u| {
+
+    $mqph = $user ? {
+      'zack'  => 1200,
+      default => 600,
+    }
+
+    mysql_user { "{$u}@localhost":
+      ensure               => present,
+      max_queries_per_hour => $mqph,
+    }
+  }
+
+  #------------------#
+#  require mysql::server
+#  mysql_user { 'zack@localhost':
+#    ensure => present,
+#    max_queries_per_hour => 1200,
+#  }
+#  mysql_user { 'monica@localhost':
+#    ensure => present,
+#    max_queries_per_hour => 600,
+#  }
+#  mysql_user { 'ralph@localhost':
+#    ensure => absent,
+#  }
+#  mysql_user { 'brad@localhost':
+#    ensure => present,
+#    max_queries_per_hour => 600,
+#  }
+#  mysql_user { 'luke@localhost':
+#    ensure => present,
+#    max_queries_per_hour => 600,
+#  }
+
+
+}
