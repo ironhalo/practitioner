4ce9bf522e7069f8b99b4e7779aaa209fb7adb71
paul <paul@puppetlabs.vm>
2016-04-26 20:19:53 +0000

    fixed selector error

--- modules/system/manifests/dbadmins.pp
+++ modules/system/manifests/dbadmins.pp
@@ -12,7 +12,7 @@ class system::dbadmins {
 
   $user.each |$u| {
 
-    $mqph = $user ? {
+    $mqph = $u ? {
       'zack'  => 1200,
       default => 600,
     }
@@ -24,26 +24,27 @@ class system::dbadmins {
   }
 
   #------------------#
-#  require mysql::server
-#  mysql_user { 'zack@localhost':
-#    ensure => present,
-#    max_queries_per_hour => 1200,
-#  }
-#  mysql_user { 'monica@localhost':
-#    ensure => present,
-#    max_queries_per_hour => 600,
-#  }
-#  mysql_user { 'ralph@localhost':
-#    ensure => absent,
-#  }
-#  mysql_user { 'brad@localhost':
-#    ensure => present,
-#    max_queries_per_hour => 600,
-#  }
-#  mysql_user { 'luke@localhost':
-#    ensure => present,
-#    max_queries_per_hour => 600,
-#  }
-
+  #  Old Code:
+  #  require mysql::server
+  #  mysql_user { 'zack@localhost':
+  #    ensure => present,
+  #    max_queries_per_hour => 1200,
+  #  }
+  #  mysql_user { 'monica@localhost':
+  #    ensure => present,
+  #    max_queries_per_hour => 600,
+  #  }
+  #  mysql_user { 'ralph@localhost':
+  #    ensure => absent,
+  #  }
+  #  mysql_user { 'brad@localhost':
+  #    ensure => present,
+  #    max_queries_per_hour => 600,
+  #  }
+  #  mysql_user { 'luke@localhost':
+  #    ensure => present,
+  #    max_queries_per_hour => 600,
+  #  }
+  #------------------#
 
 }
