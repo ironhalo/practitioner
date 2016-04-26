7e4782ca1b8b6d349c43673955fb4830a70549e9
paul <paul@puppetlabs.vm>
2016-04-26 21:52:17 +0000

    added host purging

---
+++ modules/system/manifests/hosts.pp
@@ -0,0 +1,32 @@
+class system::hosts {
+
+  resources { 'host':
+    purge => 'true',
+  }
+
+  host { 'localhost':
+    ensure       => 'present',
+    host_aliases => ['localhost.localdomain', 'localhost6', 'localhost6.localdomain6'],
+    ip           => '::1',
+    target       => '/etc/hosts',
+  }
+  host { 'master.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['master'],
+    ip           => '10.0.10.2',
+    target       => '/etc/hosts',
+  }
+  host { 'paul.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['paul'],
+    ip           => '172.16.68.130',
+    target       => '/etc/hosts',
+  }
+  host { 'student.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['training', 'localhost', 'localhost.localdomain', 'localhost4'],
+    ip           => '127.0.0.1',
+    target       => '/etc/hosts',
+  }
+
+}
